ae5652ac0a73ea7a5df3c990bc3cb02db553c458
Matti1110 <152621354+Matti1110@users.noreply.github.com>
2023-12-05 14:00:07 +0000

    qwerty

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Ava":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . e e e e e e . . . . . 
+. . . . e e d d d d e e . . . . 
+. . . . e d d d d d d e . . . . 
+. . . . e d f d d f d e . . . . 
+. . . . e d d d d d d e . . . . 
+. . . . e e d d d d e e . . . . 
+. . . . . 7 7 7 7 7 7 . . . . . 
+. . . . . 7 7 7 7 7 7 . . . . . 
+. . . . . 7 a a a a 7 . . . . . 
+. . . . . d a . . a d . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
